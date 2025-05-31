5ea1b95c8071a5c74a260c14b5590582592aaa7c
AndyBS1 <andy230505@gmail.com>
2025-05-31 12:32:15 -0600

    Seleccionar palabra random

--- main.pl
+++ main.pl
@@ -12,5 +12,12 @@ palabra('lenguaje').
 palabra('tortuga').
 palabra('regalo').
 
+elemento_en_posicion(0, [H|_], H).
+elemento_en_posicion(Pos, [_|RestoLista], Elemento) :- Pos > 0, PosAnterior is Pos - 1, elemento_en_posicion(PosAnterior, RestoLista, Elemento).
+
 :- use_module(library(random)).
 
+seleccionar_palabra_random(PalabraSeleccionada) :- findall(Palabra, palabra(Palabra), ListaPalabras),
+    length(ListaPalabras, TotalPalabras),
+    random(0, TotalPalabras, NumRandom),
+    elemento_en_posicion(NumRandom, ListaPalabras, PalabraSeleccionada).
