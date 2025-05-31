ce33d3994b6679f08d3d0e266e86e9f5eba6208a
AndyBS1 <andy230505@gmail.com>
2025-05-31 12:12:10 -0600

    palabras para la base de conocimiento

---
+++ main.pl
@@ -0,0 +1,16 @@
+% Proyecto 3 de Lenguajes de Programación
+% Andrés Bonilla y Emily Sanchez
+
+palabra('clase').
+palabra('esqueleto').
+palabra('guitarra').
+palabra('presente').
+palabra('tercero').
+palabra('biblioteca').
+palabra('tatuaje').
+palabra('lenguaje').
+palabra('tortuga').
+palabra('regalo').
+
+:- use_module(library(random)).
+
